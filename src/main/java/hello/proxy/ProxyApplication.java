c81403425e675a713ca29a116e6405898bca3140
KDH_Notebook <destiny1017@nate.com>
2023-01-15 20:52:50 +0900

    프록시 팩토리 적용

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,9 +1,8 @@
 package hello.proxy;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
-import hello.proxy.config.v2DynamicProxy.DynamicProxyBasicConfig;
-import hello.proxy.config.v2DynamicProxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v2_dynamicProxy.DynamicProxyFilterConfig;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +13,9 @@ import org.springframework.context.annotation.Import;
 //@Import({AppV1Config.class, AppV2Config.class})
 //@Import(DynamicProxyBasicConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
-@Import(DynamicProxyFilterConfig.class)
+//@Import(DynamicProxyFilterConfig.class)
+//@Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
--- src/main/java/hello/proxy/config/v2_dynamicProxy/handler/LogTraceBasicHandler.java
+++ src/main/java/hello/proxy/config/v2_dynamicProxy/handler/LogTraceBasicHandler.java
@@ -1,33 +1,23 @@
-package hello.proxy.config.v2DynamicProxy.handler;
+package hello.proxy.config.v2_dynamicProxy.handler;
 
 import hello.proxy.trace.TraceStatus;
 import hello.proxy.trace.logtrace.LogTrace;
-import org.springframework.util.PatternMatchUtils;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
-public class LogTraceFilterHandler implements InvocationHandler {
+public class LogTraceBasicHandler implements InvocationHandler {
 
     private final Object target;
     private final LogTrace logTrace;
-    private final String[] patterns;
 
-    public LogTraceFilterHandler(Object target, LogTrace logTrace, String...
-            patterns) {
+    public LogTraceBasicHandler(Object target, LogTrace logTrace) {
         this.target = target;
         this.logTrace = logTrace;
-        this.patterns = patterns;
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        //메서드 이름 필터
-        String methodName = method.getName();
-        if (!PatternMatchUtils.simpleMatch(patterns, methodName)) {
-            return method.invoke(target, args);
-        }
-
         TraceStatus status = null;
         try {
             String message = method.getDeclaringClass().getSimpleName() + "."
@@ -36,7 +26,9 @@ public class LogTraceFilterHandler implements InvocationHandler {
 
             //로직 호출
             Object result = method.invoke(target, args);
+
             logTrace.end(status);
+
             return result;
         } catch (Exception e) {
             logTrace.exception(status, e);
